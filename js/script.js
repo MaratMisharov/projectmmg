0944cdc51409ff980e966bf90f1643e9c37fcaa9
MaratMeg <netbsd@mail.ru>
2023-08-29 18:23:36 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-// let a = 16;
-
-// // >= <= == !=
-// if (a >= 9) {
-//     // true
-//     console.log('yes');
-// }
-// else {
-//         console.log('else');
-// }
-
-// const button = document.querySelector('button');
-
 
