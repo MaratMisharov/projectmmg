76b4d43faeacdd979c96e186296cb5840c74af43
MaratMeg <netbsd@mail.ru>
2023-08-31 15:26:15 +0300

    31.08.2023

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-let userAge = 36;
-//console.log(userAge);
-//console.log(typeof userAge);
-let userInfo = "Фрилансер ${userAge}";
-console.log(userInfo);
-console.log(typeof userInfo);
+// let userAge = 36;
+// //console.log(userAge);
+// //console.log(typeof userAge);
+// let userInfo = "Фрилансер ${userAge}";
+// console.log(userInfo);
+// console.log(typeof userInfo);
 
-let userHight = 145/ 0;
-console.log(userHight);
-console.log(typeof userHight);
+// let userHight = 145/ 0;
+// console.log(userHight);
+// console.log(typeof userHight);
 
-let userName;
-console.log(userName);
+// let userName;
+// console.log(userName);
 
-let userSize = "45" / "8";
-console.log(userSize);
-console.log(typeof userSize);
+// let userSize = "45" / "8";
+// console.log(userSize);
+// console.log(typeof userSize);
+
